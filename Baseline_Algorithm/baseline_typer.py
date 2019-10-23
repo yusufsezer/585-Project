e49b923fcd46169118d6ceefd2374cdb7c7ec8b7
Yusuf <yusufksezer@hotmail.com>
2019-10-23 16:04:32 -0400

    Added rule-based baseline algorithm

---
+++ Baseline_Algorithm/baseline_typer.py
@@ -0,0 +1,64 @@
+import os
+import re
+
+variables = {}
+types = []
+
+def evaluate_context(phrase, var_name):
+    other_var_match = r'^[a-z, A-Z, \-, \_]+'
+    type_matches = {
+        '$number$': r'^\(*[0-9]+\)*;?',
+        '$string$': r'''^(["'])((?:\\\1|(?:(?!\1)).)*)(\1);?''', # works with single or double quotes
+        '$bool$': r'^(true)|(false);?',
+        '$list$': r'^\['
+    }
+    pred = 'O'
+    for var_type, regex in type_matches.items():
+        if re.search(other_var_match, phrase) and not re.search(type_matches['$bool$'], phrase):
+            if phrase in variables:
+                pred = variables[phrase]
+            else:
+                if not variables.get(var_name):
+                    pred = '$any$'
+        elif re.search(regex, phrase):
+            pred = var_type
+            break
+        elif not variables.get(var_name):
+            pred = '$any$'
+    return pred
+
+def typer(tokens):
+    # Read in tokens
+    for i in range(len(tokens)):
+        if tokens[i] != '=' or i <= 0 or i >= len(tokens)-1:
+            if variables.get(tokens[i]):
+                types.append(variables[tokens[i]])
+            else:
+                types.append('O')
+            continue
+        var_name, phrase = tokens[i-1], tokens[i+1]
+
+        type_pred = evaluate_context(phrase, var_name)
+        if type_pred == "$list$" and i+2 < len(tokens):
+            type_pred = evaluate_context(tokens[i+2], var_name)[:-1] + "[]$"
+        variables[var_name] = type_pred
+        types[-1] = variables[var_name]
+        types.append('O')
+    return types
+
+if __name__ == "__main__":
+    dir = os.getcwd()
+
+    with open("500tech__angular-tree-component.json") as f:
+        tokens_and_types = [line.split('\t') for line in f.readlines()]
+        data = [{"tokens": tokens.split(' '), "types": types.split(' ')} for tokens, types in tokens_and_types]
+
+    accuracies = []
+    for i, file in enumerate(data):
+        tokens = file['tokens']
+        types  = file['types']
+        types_pred = typer(tokens)
+        correct = [pred for type, pred in zip(types, types_pred) if type == pred and type != 'O']
+        accuracies.append(len(correct) / len([t for t in types if t != ""]))
+        print(f'{len(correct)}/{len([t for t in types if t != ""])} types correctly annotated, excluding non-types')
+    print(f"Average accuracy: {sum(accuracies)/len(accuracies)}")
