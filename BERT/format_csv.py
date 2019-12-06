b6479296d4f93e7a3d2e3b07c81ebb8ae785cef4
mattrossman <mrossman@umass.edu>
2019-12-05 21:25:38 -0500

    Attempt passing special symbols thru to tags col

--- BERT/format_csv.py
+++ BERT/format_csv.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import itertools
+from typing import Tuple, List
 import os
 
 
 def flatten(list_of_lists):
     """ [['a', 'b'], ['c']] -> ['a', 'b', 'c'] """
     return [item for sublist in list_of_lists for item in sublist]
+
+
+def pass_through_symbols(toks_and_tags):
+    """Replace some of the 'O' tags taking into account Javascript syntax"""
+    df = pd.DataFrame(toks_and_tags).T
+    df.columns = ['toks', 'tags']
+    df[df.toks.isin(list('{}()'))]['tags'] = df['toks']
+    return df.values.T
+
+
+def file_len(fname):
+    i = 0
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 
 
 def parse_repo_file(json_path):
@@ -21,8 +38,14 @@ def parse_repo_file(json_path):
         ...
     ]
     """
+    num_files = file_len(json_path)
     with open(json_path) as file:
-        return [tuple(chunk.split() for chunk in line.split('\t')) for line in file]
+        out = []
+        for i, line in enumerate(file):
+            print(f'[{(i + 1) / num_files * 100:.0f}] Processing {json_path}\r', end='')
+            out.append(pass_through_symbols(tuple(chunk.split() for chunk in line.split('\t'))))
+        print()
+        return out
 
 
 def parse_all_repo_files(data_dir):
@@ -41,8 +64,8 @@ def data_to_ner_df(data):
 if __name__ == '__main__':
     data_dir = '../outputs-all'
     test_repo = f'{data_dir}/0xProject__0x.js.json'
-    data = parse_all_repo_files(data_dir)
-    df = data_to_ner_df(data)
-    ffinv = lambda s: s.mask(s == s.shift())
-    df['Sentence #'] = ffinv(df['Sentence #'])
-    df.to_csv('types_dataset_utf8.csv', index=False)
+    # data = parse_all_repo_files(data_dir)
+    # df = data_to_ner_df(data)
+    # ffinv = lambda s: s.mask(s == s.shift())
+    # df['Sentence #'] = ffinv(df['Sentence #'])
+    # df.to_csv('types_dataset_plus_utf8.csv', index=False)
